cd768b55ea008eea4f5a2a2b2b00d76314688416
prabha666 <108609740+prabha666@users.noreply.github.com>
2022-09-10 12:11:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+class App extends Component {	
+   constructor(props) {
+      super(props);
+      this.state = {
+         text:""
+      }
+   };
+   render() {
+      const para = this.state.text.length === 0 ? null : <p id = "para">{ this.state.text }</p>
+      return(
+         <div id="main">
+            {para}
+            <button id = "click" onClick = {this.handleButton.bind(this)}>Submit</button>
+         </div>
+      );
+   }
+}
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+App.prototype.handleButton = function() {
+   this.setState({
+      text : "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"   
+   })
 }
 
 
